9f64d7e7b95d8df8db221050979f3771eb128324
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-09-25 11:45:35 -0300

    Criando um novo componente no projeto: 'Book.vue'

---
+++ vue-pwa/src/components/Book/book.js
@@ -0,0 +1,11 @@
+/**
+ * Arquivo: book.js
+ * Descrição: arquivo responsável pelo script do componente: Book.vue
+ * Data: 25/09/2018
+ * Autor: Glaucia Lemos
+ */
+
+export default {
+  props: ['list'],
+  name: 'book',
+};
